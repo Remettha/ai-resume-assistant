8fb2c5ccc3a29d4d0145438616a53a3ebb446d92
Remettha <remetthabalu2006@gmail.com>
2026-09-01 19:24:09 +0530

    Update rag_service.py

--- backend/rag_service.py
+++ backend/rag_service.py
@@ -1,11 +1,11 @@
-import os
+import streamlit as st
 from google import genai
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
+    api_key=st.secrets["GEMINI_API_KEY"]
 )
 
 
